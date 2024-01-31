ee0975426c1c3ac4e49036d1e53e9767df992c67
AdamHamzahN <adamhamzahofficial13@gmail.com>
2024-01-31 10:24:37 +0700

    1/31/2024

--- resources/views/admin/login/index.blade.php
+++ resources/views/admin/login/index.blade.php
@@ -13,24 +13,21 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-12 mx-auto">
-                <div class="col-lg-6 mx-auto ">
-                    <div class="card mx-auto display-flex">
+                <div class="col-lg-6 mx-auto login-card">
+                    <div class="card mx-auto display-flex "> <!-- Add mt-5 class for top margin -->
                         <div class="card-header card-header text-center bg-white">
                             <h1 class="text-black text-login">Login Admin</h1>
                         </div>
                         <div class="card-body">
                             <form action="{{ url('admin/check') }}" method="post">
                                 <div class="form-group">
-                                    <label for="nama_admin " class="mb-3">Nama :</label>
-                                    <input type="text" name="nama_admin" id="nama_admin" class="form-control"
-                                        required><br>
+                                    <label for="nama_admin" class="mb-3">Nama :</label>
+                                    <input type="text" name="nama_admin" id="nama_admin" class="form-control login-input" required><br>
                                 </div>
                                 <div class="form-group">
                                     <label for="password" class="mb-3">Password :</label><br>
-                                    <input type="text" name="password" id="password" class="form-control"
-                                        required><br>
+                                    <input type="text" name="password" id="password" class="form-control login-input" required><br>
                                 </div>
-
                             </form>
                         </div>
                     </div>
